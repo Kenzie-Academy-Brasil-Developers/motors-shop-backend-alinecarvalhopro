e131a61e29db4e68b156b94b60e89ad30ce7f189
aline <alinecarvalho.pro@gmail.com>
2023-10-29 12:16:08 -0300

    feat: deploy configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,19 +5,22 @@ import "dotenv/config";
 const DataSourceConfig = (): DataSourceOptions => {
   const entitiesPath = path.join(__dirname, "entities/**.{js,ts}");
   const migrationsPath = path.join(__dirname, "migrations/**.{js,ts}");
+  const nodeEnv: string = process.env.NODE_ENV;
 
   if (!process.env.DATABASE_URL) {
     throw new Error("Env var DATABASE_URL does not exists");
   }
 
-  return {
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    synchronize: false,
-    logging: true,
-    entities: [entitiesPath],
-    migrations: [migrationsPath],
-  };
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      synchronize: false,
+      logging: true,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
 };
 
 const AppDataSource: DataSource = new DataSource(DataSourceConfig());
