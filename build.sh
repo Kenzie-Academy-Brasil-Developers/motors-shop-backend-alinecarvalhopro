c3e8c4b6287492df548b733def36bcea489d5acf
aline <alinecarvalho.pro@gmail.com>
2023-10-29 13:55:22 -0300

    fix: build

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-# Exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-cd /opt/render/project
 yarn
-cd src
 yarn build
 yarn typeorm migration:run -d dist/data-source
